b94dcf0997842bd57e695e8a31941e49f08d07bf
ben <ben@ubuntu.(none)>
2014-04-06 19:49:56 -0700

    fixed syntax errors, added a test

--- wiki.go
+++ wiki.go
@@ -17,11 +17,18 @@ func (p *Page) save() error {
 	return ioutil.WriteFile(filename, p.Body, 0600)
 }
 
-func loadPage(title string) *Page {
+func loadPage(title string) (*Page, error) {
 	filename := title + ".txt"
 	body, err := ioutil.ReadFile(filename)
 	if err != nil {
 		return nil, err
 	}
-	return &Page{Title: title, Body: body}
+	return &Page{Title: title, Body: body}, nil
+}
+
+func main() {
+	p1 := &Page{Title: "Index", Body: []byte("Welcome to the Wiki!")}
+	p1.save()
+	p2, _ := loadPage("Index")
+	fmt.Println(string(p2.Body))
 }
