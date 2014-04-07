7341d8651d10ffac50fd0d35bf5088d660796c15
ben <ben@ubuntu.(none)>
2014-04-06 20:06:23 -0700

    added net/http package to serve static web pages

--- wiki.go
+++ wiki.go
@@ -3,11 +3,19 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"net/http"
 )
 
 type Page struct {
 	Title string
 	Body []byte
+}
+
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/view/"):]
+	p, _ := loadPage(title)
+	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
 }
 
 // Input: pointer to a Page
@@ -17,6 +25,8 @@ func (p *Page) save() error {
 	return ioutil.WriteFile(filename, p.Body, 0600)
 }
 
+// Input: The title of a page
+// Output: A pointer to a page
 func loadPage(title string) (*Page, error) {
 	filename := title + ".txt"
 	body, err := ioutil.ReadFile(filename)
@@ -27,8 +37,6 @@ func loadPage(title string) (*Page, error) {
 }
 
 func main() {
-	p1 := &Page{Title: "Index", Body: []byte("Welcome to the Wiki!")}
-	p1.save()
-	p2, _ := loadPage("Index")
-	fmt.Println(string(p2.Body))
+	http.HandleFunc("/view/", viewHandler)
+	http.ListenAndServe(":8000", nil)
 }
