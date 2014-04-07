649047356b5a313cf219505b09c302d8709bd9e7
ben <ben@ubuntu.(none)>
2014-04-06 19:04:10 -0700

    Implemented saving and loading a simple page

---
+++ wiki.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+)
+
+type Page struct {
+	Title string
+	Body []byte
+}
+
+// Input: pointer to a Page
+// Output: Returns an "error" indicating result of writing the page to file.
+func (p *Page) save() error {
+	filename := p.Title + ".txt"
+	return ioutil.WriteFile(filename, p.Body, 0600)
+}
+
+func loadPage(title string) *Page {
+	filename := title + ".txt"
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}
+}
